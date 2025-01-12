b8caf21504b5de50eacf1a3d528d339859b5eba2
world-11 <temavetrov1@mail.ru>
2025-01-13 01:17:25 +0300

    made 3 homework

--- GameBase/task1/admin.py
+++ GameBase/task1/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Game, Buyer
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size',)
+    search_fields = ('title',)
+    list_filter = ('size', 'cost',)
+    list_per_page = 20
+
+@admin.register(Buyer)
+class BayerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age',)
+    list_filter = ('balance', 'age',)
+    search_fields = ('name',)
+    list_per_page = 30
+    fieldsets = (
+        (None, {
+        'fields': ('name', 'age')
+        }),
+        ('Дополнительные настройки', {
+            'fields': ('balance', )
+        })
+    )
+    readonly_fields = ('balance',)
+
